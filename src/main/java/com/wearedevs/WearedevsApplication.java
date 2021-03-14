1a6571dee8b3ff7f078222534c3a2c0985897860
최상현 <itchoi0429@gmail.com>
2021-03-14 14:50:18 +0900

    first commit

---
+++ src/main/java/com/wearedevs/WearedevsApplication.java
@@ -0,0 +1,13 @@
+package com.wearedevs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WearedevsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WearedevsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wearedevs/WearedevsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wearedevs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WearedevsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
