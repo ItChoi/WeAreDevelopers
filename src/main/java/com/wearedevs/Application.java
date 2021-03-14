296bdd06ade549967a2cb4a32efe002b2d44fedf
최상현 <itchoi0429@gmail.com>
2021-03-14 15:35:19 +0900

    add

--- src/main/java/com/wearedevs/Application.java
+++ src/main/java/com/wearedevs/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WearedevsApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(WearedevsApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
