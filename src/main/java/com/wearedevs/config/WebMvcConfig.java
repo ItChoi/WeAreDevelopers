e8502cd4a141db5e27c28d2a578c82f3495e2176
최상현 <itchoi0429@gmail.com>
2021-04-08 23:33:32 +0900

    add config

---
+++ src/main/java/com/wearedevs/config/WebMvcConfig.java
@@ -0,0 +1,12 @@
+package com.wearedevs.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+
+}
